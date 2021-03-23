d0d06d5a18ab998ff31509ea08b4c7d96a3945f5
Ido Leybovitch <idoleybo01@gmail.com>
2021-03-23 21:19:32 +0200

    add date function
    function to get the date in normal format with am/pm

--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,6 +1,26 @@
 import React from "react";
 
 export default function Ticket({ ticket, hideTicket }) {
+  const date = new Date(ticket.creationTime);
+  const dateFormat = () => {
+    return (
+      (date.getMonth() > 8
+        ? date.getMonth() + 1
+        : "0" + (date.getMonth() + 1)) +
+      "/" +
+      (date.getDate() > 9 ? date.getDate() : "0" + date.getDate()) +
+      "/" +
+      date.getFullYear() +
+      ", " +
+      (date.getHours() <= 9 ? "0" + date.getHours() : date.getHours()) +
+      ":" +
+      (date.getMinutes() <= 9 ? "0" + date.getMinutes() : date.getMinutes()) +
+      ":" +
+      (date.getSeconds() <= 9 ? "0" + date.getSeconds() : date.getSeconds()) +
+      " " +
+      (date.getHours() < 12 ? "AM" : "PM")
+    );
+  };
   return (
     <div className="ticket">
       <h1>{ticket.title}</h1>
@@ -8,18 +28,18 @@ export default function Ticket({ ticket, hideTicket }) {
         hide
       </button>
       <p>{ticket.content}</p>
-      <p className="information">
-        {ticket.email} | {ticket.creationTime}
-      </p>
-      <span>
-        {ticket.labels.map((label) => {
-          return (
-            <button key={`${ticket.title}-${label}`} className="label">
-              {label}
-            </button>
-          );
-        })}
-      </span>
+      <div className="details">
+        <span>{ticket.userEmail}</span> | <span>{dateFormat()}</span>
+        <span>
+          {ticket.labels.map((label, i) => {
+            return (
+              <button key={label + i} className="label">
+                {label}
+              </button>
+            );
+          })}
+        </span>
+      </div>
     </div>
   );
 }
