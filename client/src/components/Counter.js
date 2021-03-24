4205872d391aac2d73ee1e2b1365f6c75f115ef7
Ido Leybovitch <idoleybo01@gmail.com>
2021-03-24 12:33:53 +0200

    trying to pass the test in github action #2
    I changed the structure of the code to make it more orderly

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -4,15 +4,19 @@ import "../styles/counter.css";
 export default function Counter({ counter, hideList, showTickets, onClick }) {
   if (hideList.length > 0) {
     return (
-      <div className="counter-section">
-        <span className="show-tickets-number">
-          Showing {showTickets} results
+      <>
+        <div className="counter-section">
+          <span className="show-tickets-number">
+            Showing {showTickets} results
+          </span>
+        </div>
+        <span>
+          <span id="hideTicketsCounter">{counter}</span> hidden ticket`s{" "}
+          <button id="restoreHideTickets" onClick={onClick}>
+            restored
+          </button>
         </span>
-        {/*<span id="hideTicketsCounter">{counter}</span> hidden ticket`s{" "}
-        <button id="restoreHideTickets" onClick={onClick}>
-          restored
-    </button>*/}
-      </div>
+      </>
     );
   } else {
     return (
